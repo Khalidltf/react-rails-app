f0260f99cb890c45e6c798c6653b7e90adddc6cd
khalidlotfi <khalidlotfi2005@gmail.com>
2024-06-08 16:24:02 +0100

    Building the React Registration Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import "./Home.css";
+import Registration from "./auth/Registration";
 
 function Home() {
   return (
     <>
       <h1>Home</h1>
+      <Registration />
     </>
   );
 }
---
+++ src/components/auth/Registration.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import axios from "axios";
+import "./Registration.css";
+
+function Registration() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    password_confirmation: "",
+    registration_errors: "",
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted ...");
+    axios
+      .post(
+        "http://127.0.0.1:3000/registrations",
+        {
+          user: {
+            email: formData.email,
+            password: formData.password,
+            password_confirmation: formData.password_confirmation,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((res) => console.log("res:", res))
+      .catch((error) => console.log("error:", error));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="email"
+        type="email"
+        value={formData.email}
+        placeholder="email"
+        onChange={handleChange}
+        required
+      />
+      <input
+        name="password"
+        type="password"
+        value={formData.password}
+        placeholder="password"
+        onChange={handleChange}
+        required
+      />
+      <input
+        name="password_confirmation"
+        type="password"
+        value={formData.password_confirmation}
+        placeholder="password confirmation"
+        onChange={handleChange}
+        required
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Registration;
